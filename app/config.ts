ac342aa45d1a1420c6aee94f2c0beb5b03f09103
ternera <ternera@users.noreply.github.com>
2025-07-02 08:29:38 -0500

    fixing signal url

--- app/config.ts
+++ app/config.ts
@@ -13,7 +13,7 @@ export const socialLinks = {
   youtube: "https://youtube.com/@ternera",
   medium: "https://medium.com/@ternera",
   bluesky: "https://bsky.app/profile/ternera.bsky.social",
-  signal: "https://signal.me/#eu/ternera.01",
+  signal: "https://signal.me/#eu/xwTrQIubDCOHK0hpPJSp-P-y9t5xGLdXRLacO6srVrC1xSrYZURPL6ZDU-w5b9lE",
   spotify: "https://open.spotify.com/user/31uyi4f227ggbuf6shvi5e7wkjai",
   matrix: "https://matrix.to/#/@ternera:matrix.org",
   linkedin: "https://linkedin.com/in/ternera",
