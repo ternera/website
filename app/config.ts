f51622ba1ff3391f2563149212b9a924e4209bb2
ternera <hi@ternera.org>
2025-07-02 18:47:27 -0500

    fixing bsky link

--- app/config.ts
+++ app/config.ts
@@ -12,7 +12,7 @@ export const socialLinks = {
   reddit: "https://reddit.com/u/ternera",
   youtube: "https://youtube.com/@ternera",
   medium: "https://medium.com/@ternera",
-  bluesky: "https://bsky.app/profile/ternera.bsky.social",
+  bluesky: "https://bsky.app/profile/ternera.org",
   signal: "https://signal.me/#eu/xwTrQIubDCOHK0hpPJSp-P-y9t5xGLdXRLacO6srVrC1xSrYZURPL6ZDU-w5b9lE",
   spotify: "https://open.spotify.com/user/31uyi4f227ggbuf6shvi5e7wkjai",
   matrix: "https://matrix.to/#/@ternera:matrix.org",
